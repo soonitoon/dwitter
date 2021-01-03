15444d5a2b1e26cc6a64957a5f5df393355dadee
soonitoon <soonitoon@gmail.com>
2021-01-04 02:19:31 +0900

    make auth frame

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
-export default () => {
+const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
       <Switch>
         {isLoggedIn ? (
           <>
-            <Route></Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
           </>
         ) : (
-          <Route>
+          <Route exact path="/">
             <Auth />
           </Route>
         )}
@@ -20,3 +23,5 @@ export default () => {
     </Router>
   );
 };
+
+export default AppRouter;
