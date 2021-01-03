a32877e73fda8d82db5be1917e4c4694e1d32039
soonitoon <soonitoon@gmail.com>
2021-01-04 02:24:03 +0900

    move login state to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
