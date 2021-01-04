b3bc41e927361cd047b46888e2f4fa283257a2df
soonitoon <soonitoon@gmail.com>
2021-01-04 20:59:23 +0900

    add twitte form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [dwitte, setDwitte] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setDwitte(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={dwitte}
+          onChange={onChange}
+          type="text"
+          placeholder="What's your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="dwitte" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
