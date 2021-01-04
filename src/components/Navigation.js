27e06cbc69ac6448a4ee5c3e4713eed8c59a1576
soonitoon <soonitoon@gmail.com>
2021-01-04 19:42:18 +0900

    add Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Navigation = () => <nav>nav</nav>;
+
+export default Navigation;
--- src/components/Router.js
+++ src/components/Router.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "/components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
