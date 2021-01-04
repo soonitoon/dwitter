99fd4834144004e5ef621a2f40ef6ee9bd40cc12
soonitoon <soonitoon@gmail.com>
2021-01-04 20:37:21 +0900

    add logout function

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { AuthService } from "mybase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogoutClick = () => {
+    AuthService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogoutClick}>log out</button>
+    </>
+  );
+};
 
 export default Profile;
