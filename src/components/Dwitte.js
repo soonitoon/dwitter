9f76f2b21a95fdefc23fa9ef82a2afa94a3b60cc
soonitoon <soonitoon@gmail.com>
2021-01-05 14:22:02 +0900

    add function to delete dwitte

--- src/components/Dwitte.js
+++ src/components/Dwitte.js
@@ -1,15 +1,25 @@
+import { DBService } from "mybase";
 import React from "react";
 
-const Dwitte = ({ dwitteObj, isOwner }) => (
-  <div>
-    <h4>{dwitteObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+const Dwitte = ({ dwitteObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure?");
+    if (ok) {
+      await DBService.doc(`dwitte/${dwitteObj.id}`).delete();
+    }
+  };
+
+  return (
+    <div>
+      <h4>{dwitteObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete</button>
+          <button>Edit</button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Dwitte;
